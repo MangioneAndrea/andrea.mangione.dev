9f20d4ccb621f5068b02a63dc9c4cd924e575702
Andrea Mangione <uremiram@gmail.com>
2023-03-06 11:10:48 +0100

    add unsteady text

---
+++ src/helpers/UnsteadyText.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { oneRandomOutOf } from "./CommonFunctions";
+
+
+  export let text= "";
+
+ let elems = Array.from(text).map((letter) => ({letter, where: oneRandomOutOf("0.5em", "-0.5em")}));
+
+</script>
+
+{#each elems as {letter, where}}
+	<span style={`--where: translateY(${where});`}>{letter}</span>
+{/each}
+
+<style>
+	span{
+		display: inline-block;
+		transition: all ease-in-out 0.5s;
+	}
+	span:hover{
+		transform: var(--where);
+	}
+</style>
