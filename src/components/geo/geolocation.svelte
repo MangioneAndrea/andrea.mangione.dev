4457e4b27d1b3cea93ec898ec0bd190b576e5bbb
Andrea Mangione <uremiram@gmail.com>
2024-07-03 07:49:39 +0200

    feat: preamble of speedmeter

---
+++ src/components/geo/geolocation.svelte
@@ -0,0 +1,64 @@
+<script>
+  import { onMount } from 'svelte'
+
+  let lat = 0
+  let lng = 0
+  let time = 0
+
+  let distance = 0
+
+  let speedKmH = 0
+  let speedMS = 0
+
+  let deltaLat = 0
+  let deltaLng = 0
+
+  onMount(async () => {
+    navigator.geolocation.getCurrentPosition(update)
+
+    navigator.geolocation.watchPosition(update)
+
+    console.log('mount')
+  })
+
+  function haversine(lat1, lon1, lat2, lon2) {
+    const R = 6371
+    const dLat = toRadians(lat2 - lat1)
+    const dLon = toRadians(lon2 - lon1)
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(toRadians(lat1)) *
+        Math.cos(toRadians(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    return R * c
+  }
+
+  function toRadians(degrees) {
+    return degrees * (Math.PI / 180)
+  }
+
+  /** @param {GeolocationPosition} pos */
+  function update(pos) {
+    deltaLat = ((lat - pos.coords.latitude) * 1000) / (pos.timestamp - time)
+    deltaLng = ((lng - pos.coords.longitude) * 1000) / (pos.timestamp - time)
+
+    distance = haversine(lat, lng, pos.coords.latitude, pos.coords.longitude)
+
+    speedKmH = distance / ((pos.timestamp - time) / (60 * 60 * 1000))
+    speedMS = distance / (pos.timestamp - time)
+
+    lat = pos.coords.latitude
+    lng = pos.coords.longitude
+    time = pos.timestamp
+  }
+</script>
+
+<div>
+  <span>Latitude: {lat} | {deltaLat}</span>
+  <br />
+  <span>Longitude: {lng} | {deltaLng}</span>
+  <br />
+  <span>Speed: {Math.round(speedKmH)}Km/h or {Math.round(speedMS)}M/s</span>
+</div>
