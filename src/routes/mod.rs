8c7587f28d72b7ebcf1ea50f9605b7b9a2f4f651
Andrea Mangione <andrea@mangione.dev>
2025-03-16 23:47:37 +0100

    messy trial with img and canvas

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -6,7 +6,6 @@ mod home;
 
 use leptos_router::components::{Route, Routes};
 use leptos_router::hooks::use_location;
-use leptos_router::location::Location;
 use leptos_router::path;
 
 use blog::Blog;
@@ -27,36 +26,21 @@ impl Routes {
         }
     }
 
-    //pub fn view(&self) -> AnyView {
-    //    match self {
-    //        Self::Home => Home().into_any(),
-    //        Self::Blog => Blog().into_any(),
-    //    }
-    //}
-    //
-
-
     pub fn get_active() -> Self {
         let p = use_location().pathname.get();
         match p.as_str() {
             "/blog" => Self::Blog,
-            _ => Self::Home
+            _ => Self::Home,
         }
     }
 }
 
 #[component]
 pub fn routes_slot() -> impl IntoView {
-    //let r:Vec<_> = Routes::iter()
-    //    .map(|r|
-    //        view! {<Route path=StaticSegment(r.path()) view=||view! {r.view()} />}
-    //    )
-    //    .collect();
-
     view! {
-            <Routes fallback=|| "404">
-                <Route path=path!("") view=Home />
-                <Route path=path!("/blog") view=Blog />
-            </Routes>
+        <Routes fallback=|| "404">
+            <Route path=path!("") view=Home />
+            <Route path=path!("/blog") view=Blog />
+        </Routes>
     }
 }
