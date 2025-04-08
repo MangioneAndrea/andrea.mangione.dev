ae0bd29a2e1ee442e4c12364aaf5dab0781d2f00
Andrea Mangione <andrea@mangione.dev>
2025-04-08 21:25:51 +0200

    chore: minor corrections

--- src/routes/home/about.rs
+++ src/routes/home/about.rs
@@ -6,8 +6,8 @@ pub fn about() -> impl IntoView {
     view! {
         <p class="lg:w-[60%] text-center content-center m-auto mt-8">
             Hi there! Welcome to my homepage ={")"} <br />I am a Swtizerland based developer. <br />
-            I work at <a href="http://coop.ch">Coop</a>
-            as a System architect and Product Owner. I pretty much work on devops for the systems present in every store in Switzerland.
+            I work at <a class="link" href="http://coop.ch">Coop</a>
+            as a System architect and Product Owner. I pretty much work on devops for the systems present in every Coop store in Switzerland.
 
             <br />
 
